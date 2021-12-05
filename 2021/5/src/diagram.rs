19fb913d476c95f3f45ec4ba3c34a09a73b7b754
Jesus Hernandez <jesushdez@protonmail.com>
2021-12-05 17:41:02 +0100

    Optimize to use less memory and allocate less times

--- 2021/5/src/diagram.rs
+++ 2021/5/src/diagram.rs
@@ -1,33 +1,44 @@
 use std::default::Default;
 use std::fmt;
 use std::cmp::{min, max};
+use std::collections::HashMap;
 
 #[derive(Default, Debug)]
 pub struct HydrothermalDiagram {
-     matrix: Vec<Vec<usize>>,
+     light_matrix: HashMap<(usize, usize), usize>,
 }
 
 impl HydrothermalDiagram {
-     fn expand_to(&mut self, x_len: usize, y_len: usize) {
-          while self.matrix.len() < y_len {
-               self.matrix.push(Vec::new());
-          }
-          for row in &mut self.matrix {
-               while row.len() < x_len {
-                    row.push(0);
-               }
+     fn update_coords(&mut self, x: usize, y: usize) {
+          if let Some(coord_total) = self.light_matrix.get_mut(&(x, y)) {
+               *coord_total += 1;
+          } else {
+               self.light_matrix.insert((x, y), 1);
           }
      }
 
-     pub fn line(&mut self, x1: usize, y1: usize, x2: usize, y2: usize) {
-          self.expand_to(max(x1, x2) + 1, max(y1, y2) + 1);
+     fn matrix_size(&self) -> (usize, usize) {
+          let x_len = self.light_matrix.keys().map(|(x, _y)| x).max().unwrap_or(&0);
+          let y_len = self.light_matrix.keys().map(|(_x, y)| y).max().unwrap_or(&0);
+          return (*x_len + 1, *y_len + 1);
+     }
+
+     fn get_coords_total(&self, x: usize, y: usize) -> usize {
+          if let Some(coord_total) = self.light_matrix.get(&(x, y)) {
+               return *coord_total;
+          } else {
+               return 0;
+          }
+     }
+
+     pub fn add_line(&mut self, x1: usize, y1: usize, x2: usize, y2: usize) {
           if x1 == x2 {
                for y in min(y1, y2)..=max(y1, y2) {
-                    self.matrix[y][x1] += 1;
+                    self.update_coords(x1, y);
                }
           } else if y1 == y2 {
                for x in min(x1, x2)..=max(x1, x2) {
-                    self.matrix[y1][x] += 1;
+                    self.update_coords(x, y1);
                }
           } else {
                let steps_x = max(x1, x2)-min(x1, x2);
@@ -36,7 +47,7 @@ impl HydrothermalDiagram {
                     for step in 0..=steps_x {
                          let x = if x1 < x2 { x1 + step } else { x1 - step };
                          let y = if y1 < y2 { y1 + step } else { y1 - step };
-                         self.matrix[y][x] += 1;
+                         self.update_coords(x, y);
                     }
                }
           }
@@ -44,29 +55,30 @@ impl HydrothermalDiagram {
 
      pub fn total_danger_points(&self) -> usize {
           let mut total = 0;
-          for row in &self.matrix {
-               for col in row {
-                    if *col > 1 {
-                         total += 1;
-                    }
+          for (_coords, coords_total) in &self.light_matrix {
+               if *coords_total > 1 {
+                    total += 1;
                }
           }
           return total;
      }
 
      pub fn x_len(&self) -> usize {
-          self.matrix[0].len()
+          let (x_len, _y_len) = self.matrix_size();
+          return x_len;
      }
 }
 
 impl fmt::Display for HydrothermalDiagram {
      fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-          for row in &self.matrix {
-               for col in row {
-                    if *col == 0 {
+          let (x_len, y_len) = self.matrix_size();
+          for y in 0..y_len {
+               for x in 0..x_len {
+                    let coords_total = self.get_coords_total(x, y);
+                    if coords_total == 0 {
                          write!(f, ".")?
                     } else {
-                         write!(f, "{}", col)?
+                         write!(f, "{}", coords_total)?
                     }
                }
                write!(f, "\n")?
--- 2021/5/src/main.rs
+++ 2021/5/src/main.rs
@@ -22,7 +22,7 @@ fn main() -> io::Result<()> {
         let line = line?;
         let (mut x1, mut y1, mut x2, mut y2) = (0usize, 0usize, 0usize, 0usize);
         sscanf!(&line, "{},{} -> {},{}", x1, y1, x2, y2)?;
-        diagram.line(x1, y1, x2, y2);
+        diagram.add_line(x1, y1, x2, y2);
     }
     if diagram.x_len() < 100 {
         println!("{}", diagram);
