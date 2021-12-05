b989f33072b1077426549c0c33e6a8cd812e4174
Jesus Hernandez <jesushdez@protonmail.com>
2021-12-05 22:26:30 +0100

    Add sliding window

---
+++ 2021/1/src/depth.rs
@@ -0,0 +1,25 @@
+use std::collections::VecDeque;
+use std::default::Default;
+
+#[derive(Debug, Default)]
+pub struct SlidingWindowDepthMeasures {
+    queue: VecDeque<usize>,
+}
+
+impl SlidingWindowDepthMeasures {
+    pub fn add(&mut self, depth_measure: usize) {
+        self.queue.push_back(depth_measure);
+        while self.queue.len() > 3 {
+            self.queue.pop_front();
+        }
+    }
+
+    pub fn measures_sum(&self) -> Option<usize> {
+        if !self.enough_len() {
+            return None;
+        }
+        return self.queue.iter().map(|&x| x).reduce(|accum, item| accum + item);
+    }
+
+    pub fn enough_len(&self) -> bool { self.queue.len() == 3 }
+}
--- 2021/1/src/main.rs
+++ 2021/1/src/main.rs
@@ -4,6 +4,9 @@ use std::path::PathBuf;
 use std::fs::File;
 use std::io::{self, BufRead};
 
+mod depth;
+use depth::SlidingWindowDepthMeasures;
+
 #[derive(StructOpt)]
 struct Opt {
     input_file: PathBuf,
@@ -18,23 +21,29 @@ impl Opt {
 }
 
 fn main() -> io::Result<()> {
+    let mut depth_measures_window = SlidingWindowDepthMeasures::default();
     let mut prev_depth_measure = None;
     let mut total_times_increase = 0;
     for line in Opt::lines()? {
         let depth_measure: usize = line?.parse().expect("Can not parse the depth measurement");
+        depth_measures_window.add(depth_measure);
+        if !depth_measures_window.enough_len() {
+            continue;
+        }
+        let measures_sum = depth_measures_window.measures_sum().expect("Without measures in the sliding window");
         if let Some(prev_depth_measure) = prev_depth_measure {
-            if prev_depth_measure < depth_measure {
-                println!("{} (increased)", depth_measure);
+            if prev_depth_measure < measures_sum {
+                println!("{} (increased)", measures_sum);
                 total_times_increase += 1;
-            } else if prev_depth_measure > depth_measure {
-                println!("{} (decreased)", depth_measure);
+            } else if prev_depth_measure > measures_sum {
+                println!("{} (decreased)", measures_sum);
             } else {
-                println!("{} (keep)", depth_measure);
+                println!("{} (keep)", measures_sum);
             }
         } else {
-            println!("{} (N/A - no previous measurement)", depth_measure);
+            println!("{} (N/A - no previous measurement)", measures_sum);
         }
-        prev_depth_measure = Some(depth_measure);
+        prev_depth_measure = Some(measures_sum);
     }
     println!("{}", total_times_increase);
     return Ok(());
